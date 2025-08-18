3632a22399ee7d429ad99ff77d89ef13b94ea9d4
jianshu93 <jansuechao@gmail.com>
2025-08-17 22:29:30 -0700

    v0.1.0

---
+++ Formula/DartUniFrac.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class DartUniFrac < Formula
+  desc "DartUniFrac: Approximate unweighted UniFrac via Weighted MinHash"
+  homepage "https://github.com/jianshu93/DartUniFrac"
+  url "https://github.com/jianshu93/DartUniFrac/releases/download/v0.1.0/dartunifrac_Darwin_universal_v0.1.0.tar.gz"
+  sha256 "7a53bffe0b15139c80f2679606db807785c7cf0771cfeca09192cdfa9d54b145"
+  version "0.1.0"
+
+  def install
+    bin.install "dartunifrac"
+  end
+end
