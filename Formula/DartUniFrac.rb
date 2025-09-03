dcb087203e5e4ba843ca1fbf4f22d125f0956c0b
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-09-02 18:29:19 -0700

    Update DartUniFrac.rb
    v0.2.3

--- Formula/DartUniFrac.rb
+++ Formula/DartUniFrac.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Dartunifrac < Formula
-  desc "DartUniFrac: Approximate unweighted UniFrac via Weighted MinHash"
+  desc "DartUniFrac: Approximate UniFrac via Weighted MinHash"
   homepage "https://github.com/jianshu93/DartUniFrac"
-  url "https://github.com/jianshu93/DartUniFrac/releases/download/v0.2.2/dartunifrac_Darwin_universal_v0.2.2.tar.gz"
-  sha256 "fbd984ae3628b0b2d70bc43a01d23371e56691745a91c0b18a7d5e482a903480"
-  version "0.2.2"
+  url "https://github.com/jianshu93/DartUniFrac/releases/download/v0.2.3/dartunifrac_Darwin_universal_v0.2.3.tar.gz"
+  sha256 "c40eb453bf301863b36561646c3ebce5d13991cab05a3e8162ba9c7cd9285391"
+  version "0.2.3"
 
   def install
     bin.install "dartunifrac"
